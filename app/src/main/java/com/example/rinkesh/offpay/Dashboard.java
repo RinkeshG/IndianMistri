9f7c3f79823bea79ceb5c999ba4801d7e14437fb
RinkeshG <saviorindiaservice@gmail.com>
2017-10-17 09:18:33 +0530

    5th Commit
    5th

--- app/src/main/java/com/example/rinkesh/offpay/Dashboard.java
+++ app/src/main/java/com/example/rinkesh/offpay/Dashboard.java
@@ -82,13 +82,20 @@ public class Dashboard extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.dashboard) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.bookservice) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.mybooking) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.wallet) {
+
+        } else if (id == R.id.Profile) {
+
+            Intent intent = new Intent(this, Profile.class);
+            startActivity(intent);
+
+        } else if (id == R.id.getincontact) {
 
         } else if (id == R.id.nav_share) {
 
